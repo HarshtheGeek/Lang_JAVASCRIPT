9fdc9857046016064e1df6b7785e3d2ed8947d07
Harsh <vishwaharsh10@gmail.com>
2024-12-02 22:08:37 +0530

    Datatypes completed

---
+++ Chapter_1/datatypes.js
@@ -0,0 +1,30 @@
+//Numbers
+let a = 3;
+let b= 4;
+console.log(a+b);
+
+//Strings
+const Name = "Harsh vishwakarma";
+console.log(Name);
+
+//Boolean
+let Isloggedin = true;
+console.log(Isloggedin);
+
+//NULL
+let lastlogindate = null;
+console.log(lastlogindate);
+
+//Undefined
+let lastsignin = undefined;
+console.log(lastsignin);
+
+//Objects
+const Person = {
+    FirstName: "Harsh",
+    LastName: "Vishwakarma",
+    Age: 20,
+    Gender: "Male",
+};
+
+console.log(Person.Age);
---
+++ Chapter_1/one.js
@@ -0,0 +1,5 @@
+//WAP TO PRINT HELLO WORLD//
+
+console.log("Hello world");
+console.log("this is my first js program");
+
