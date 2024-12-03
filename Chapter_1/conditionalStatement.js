45646486d2af82fb2f925b7d8666de2d3991da0e
Harsh <vishwaharsh10@gmail.com>
2024-12-03 14:10:27 +0530

    Conditional statement done

---
+++ Chapter_1/conditionalStatement.js
@@ -0,0 +1,32 @@
+// Conditinal staetements in Javascript //
+
+//if-else satement 
+const age = 20;
+
+if(age>=20){
+    console.log("You can vote for the country");
+}
+else{
+    console.log("You cannot vote for the country");
+}
+
+
+//Ternary operator
+const Age = 20
+Age>=20 ? console.log("ELIGIBLE TO VOTE") : console.log("NOT ELIGIBLE TO VOTE");
+
+
+
+//if else and elif statement
+if(age>=20){
+    console.log("You can vote for the country");
+}
+else if(age<=18){
+    console.log("You are underage to vote for the country");
+}
+else{
+    console.log("You are adult and you can vote");
+}
+
+
+
