69ff2045a5ba5cc0ce25696b388bb6eb94c24b4f
Harsh <vishwaharsh10@gmail.com>
2024-12-04 10:41:29 +0530

    functions started

---
+++ Chapter_2/Functions.js
@@ -0,0 +1,17 @@
+//Functions 
+
+function add(a,b){
+    console.log(a+b);
+}
+add(10,20);
+
+
+function multiply(a,b){
+    console.log(a*b);
+}
+multiply(10,20)
+
+
+
+//WAP a program to add multiple numbers using functions
+
