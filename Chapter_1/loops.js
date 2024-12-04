0def7bc86cdd37a5306f107c7ff06e44de2f4d67
Harsh <vishwaharsh10@gmail.com>
2024-12-04 10:26:16 +0530

    Loops completed

---
+++ Chapter_1/loops.js
@@ -0,0 +1,27 @@
+//WAP to impliment loops in a javascript//
+
+
+//WAP for the sum of numbers
+let arr = [1,2,3,4,5];
+let sum = 0 ;
+
+for(let i=0;i<5;i++){
+    sum = sum+arr[i];
+    
+}
+console.log("Sum",sum);
+
+
+//WAP to print even number
+let num = 20;
+for (let i=0; i<num ; i++){
+    if(i % 2===0){
+        console.log("Your output is",i);
+    }
+}
+
+
+
+
+
+
