34aecdf6e662cd870a30b0b9569aa7fba0e85835
Harsh <vishwaharsh10@gmail.com>
2024-12-11 23:13:26 +0530

    High order functions completed

--- Chapter_3/High_order_functions.js
+++ Chapter_3/High_order_functions.js
@@ -1,3 +1,33 @@
 //HIGH ORDER FUNCTIONS - These are the functions that take functions as an argument
 
-// foreach(High order function // 
+//(High order function)//
+
+//For normal functions
+function add(a,b,cb){
+    let result= a+b;
+    cb(result);
+}
+add(10,20,function(val){
+    console.log(val);
+})
+
+
+//For arrow functions
+function add(a, b, cb) {
+    let result = a + b; // Calculate the sum of a and b
+    cb(result); // Call the callback function with the result
+}
+add(10, 20, (val) => console.log(val));
+
+
+//Return function inside a function
+function add(a, b, cb) {
+    let result = a + b; 
+    cb(result);
+    return () => console.log(result);
+}
+let ResultFunctions= add(10, 20, () => {});
+ResultFunctions();
+
+
+
